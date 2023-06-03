b78199d17544c30ea3a0fc5fc2396c728916eba6
chaseknis <chase6th@gmail.com>
2023-06-03 16:21:09 +0200

    Add features tests

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,5 @@
 class Expense < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
-  belongs_to :groups
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :name, presence: true
